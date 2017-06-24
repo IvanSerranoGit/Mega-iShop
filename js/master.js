3390993c63e6b20c9a611f5db184c601341d202b
Ivan Serrano <contacto.ivanserrano@gmail.com>
2017-06-23 23:03:34 -0500

    Mega iShop

---
+++ js/master.js
@@ -0,0 +1,49 @@
+const $form = $('#suscribeForm')
+const $formGroup = $form.find('.form-group')
+const $formControl = $formGroup.find('.form-control')
+
+$form.submit((ev) => {
+  ev.preventDefault()
+  
+  const error = Math.random() > 0.5
+  $formGroup.removeClass().addClass('form-group')
+  $formControl.removeClass().addClass('form-control')
+  $formGroup.addClass(error ? 'has-danger' : 'has-success')
+  $formControl.addClass(error ? 'form-control-danger' : 'form-control-success')
+  $formGroup.find('.form-control-feedback').remove()
+
+  var el;
+  if (error) {
+    el = $('<div>', { html: 'Ha ocurrido un error' })
+  } else {
+    el = $('<div>', { html: 'Te enviaremos todas las novedades!' })
+  }
+  el.addClass('form-control-feedback')
+  $formGroup.append(el)
+})
+
+function mostrarModal(){
+  var storage = localStorage.noMostrarModal;
+  var noMostrarModal = (storage===undefined) ? false : JSON.parse(storage);
+
+  if (!noMostrarModal){
+    $("#modalOferta").modal();
+  }
+    
+  $("#btnNoRegistrar").click(function(ev){
+    localStorage.noMostrarModal = true
+  });
+}
+
+const $filtrosToggle = $('#filtrosToggle')
+$filtrosToggle.click((ev) => {
+  ev.preventDefault()
+
+  const $i = $filtrosToggle.find('i.fa')
+  const isDown = $i.hasClass('fa-chevron-down')
+  if (isDown) {
+    $i.removeClass('fa-chevron-down').addClass('fa-chevron-up')
+  } else {
+    $i.removeClass('fa-chevron-up').addClass('fa-chevron-down')
+  }
+})
